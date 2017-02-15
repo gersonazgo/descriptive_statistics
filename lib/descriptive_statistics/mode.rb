934874bf250994a09dcb9851e6ac0859c26bf1a3
Gerson Azevedo <gersonazgo@gmail.com>
2017-02-14 22:02:52 -0200

    Changing the mode algorythm

--- lib/descriptive_statistics/mode.rb
+++ lib/descriptive_statistics/mode.rb
@@ -3,10 +3,25 @@ module DescriptiveStatistics
     values = Support::extract(collection, &block)
     return if values.to_a.empty?
 
-    values
+    mode_count = values
       .group_by { |e| e }
       .values
       .max_by(&:size)
-      .first
+      .count
+
+    return if mode_count == 1
+
+    modes = 0 
+
+    values.group_by { |e| e }.values.each do |elements|
+        if elements.count == mode_count
+            modes += 1
+        end 
+    end
+
+    return if modes >=2
+
+    return values.group_by { |e| e }.values.max_by(&:size).first
+
   end
 end
